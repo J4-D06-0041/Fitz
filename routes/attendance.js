d135a64c9af397d2b432e56af29fc76c361f8103
Jero-BFT <jerod@bigforktech.com>
2024-06-11 10:57:33 +0800

    basic attendanceRoute

---
+++ routes/attendance.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const authMiddleware = require("../middlewares/authMiddleware");
+const { logAttendance, getUserAttendance, getTeamAttendance } = require("../controllers/attendanceController");
+
+router.post("/log", authMiddleware, logAttendance);
+router.get("/user/:userId", authMiddleware, getUserAttendance);
+router.get("/team", authMiddleware, getTeamAttendance);
+
+module.exports = router;
