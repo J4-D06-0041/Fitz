6dff48711507e13b41ad5131d9ed323a1170f663
Jero-BFT <jerod@bigforktech.com>
2024-06-29 02:50:30 +0800

    KAN-9
    updated the attendanceService.js to utilize logger
    updated the attributes needed in saving a new entry as well

--- services/attendanceService.js
+++ services/attendanceService.js
@@ -1,32 +1,31 @@
 const Attendance = require("../models/Attendance");
 const User = require("../models/User");
 const moment = require("moment");
+const logger = require("../logger");
 
 class AttendanceController {
   constructor() {}
 
-  async logAttendance(userId, loginTime, logoutTime, totalTime, phTimeLogin, phTimeLogout, dateCreated, dateUpdated) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const attendance = new Attendance({
-          user: userId,
-          loginTime,
-          logoutTime,
-          totalTime,
-          phTimeLogin,
-          phTimeLogout,
-          dateCreated,
-          dateUpdated,
-        });
+  async logAttendance(attendanceObject) {
+    try {
+      logger.info("Received in logAttendance:", attendanceObject);
+      const attendance = new Attendance({
+        user: attendanceObject.user,
+        clientLoginTime: attendanceObject.clientLoginTime,
+        clientLogoutTime: attendanceObject.clientLogoutTime,
+        userLoginTime: attendanceObject.userLoginTime,
+        userLogoutTime: attendanceObject.userLogoutTime,
+        dateCreated: attendanceObject.dateCreated,
+        totalWorkTime: attendanceObject.totalWorkTime,
+      });
 
-        await attendance.save();
-
-        resolve(attendance);
-      } catch (err) {
-        console.error(err.message);
-        reject(err);
-      }
-    });
+      await attendance.save();
+      logger.info("Attendance logged successfully:", attendance.toObject());
+      return attendance.toObject(); // Returning the document directly
+    } catch (err) {
+      logger.error(err.stack);
+      throw err; // Rethrow or handle as needed
+    }
   }
 
   async getUserAttendance(userId) {
