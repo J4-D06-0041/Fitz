6c5ae8be02cfd315819a87d5b8ace9bd004bdd00
Jero-BFT <jerod@bigforktech.com>
2024-06-29 02:49:18 +0800

    KAN-3
    updated user route to have a function to check for user role based on logged in user

--- routes/users.js
+++ routes/users.js
@@ -2,17 +2,43 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middlewares/authMiddleware");
 const { getAllUsers, getUserById, updateUser, deleteUser } = require("../services/userService");
+const logger = require("../logger");
+const jwt = require("jsonwebtoken");
 
 // Route to get all users
 router.get("/", authMiddleware, getAllUsers);
 
 // Route to get a specific user by ID
-router.get("/:id", authMiddleware, getUserById);
+router.get("/getuser/:id", authMiddleware, getUserById);
 
 // Route to update user details
 router.put("/:id", authMiddleware, updateUser);
 
 // Route to delete a user
 router.delete("/:id", authMiddleware, deleteUser);
+
+router.get("/get-user-role", authMiddleware, (req, res) => {
+  try {
+    logger.info("Inside get-user-role");
+    const token = req.cookies.token;
+    logger.info(`token from get-user-role ${token}`);
+    if (!token) {
+      return res.status(401).send({ error: "No token provided" });
+    }
+
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      logger.info(`decoded from get-user-role ${JSON.stringify(decoded)}`);
+      return res.status(200).send({ role: decoded.user.role });
+      // return res.status(200).send({ role: decoded.role });
+    } catch (error) {
+      logger.error(`error from get-user-role ${error}`);
+      return res.status(401).send({ error: "Invalid token" });
+    }
+  } catch (error) {
+    logger.error(`error from get-user-role ${error}`);
+    return res.status(500).send({ error: "Server error" });
+  }
+});
 
 module.exports = router;
