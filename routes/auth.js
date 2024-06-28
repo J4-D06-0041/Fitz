e2fb696d24f8d79368b5e194f6bb0d0a86696144
Jero-BFT <jerod@bigforktech.com>
2024-06-28 20:35:56 +0800

    KAN-12
    fixed auth to utilize cookies instead of localStorage

--- routes/auth.js
+++ routes/auth.js
@@ -40,49 +40,60 @@ router.post("/login", async (req, res) => {
     let user = await User.findOne({ username });
 
     if (!user) {
-      return res.status(400).json({ msg: "Invalid credentials" });
+      return res.status(401).send("Invalid credentials");
+    } else {
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(401).send("Invalid credentials");
+      }
+      //add login time
+      const userOffset = user.timezone;
+      const phTimeLogin = moment().tz("Asia/Singapore").add(8, "hours").format("YYYY-MM-DD HH:mm:ss");
+      const tzConverter = -8;
+      const userTimeLogin = moment()
+        .utcOffset(userOffset * 60)
+        .subtract(tzConverter, "hours")
+        .format("YYYY-MM-DD HH:mm:ss");
+      const attendance = new Attendance({
+        user: user.id,
+        loginTime: userTimeLogin,
+        phTimeLogin: phTimeLogin,
+        logoutTime: null,
+        phTimeLogout: null,
+        totalTime: 0,
+      });
+
+      //add entry to attendance table
+      await attendance.save();
+
+      console.log("Updated attendance record:", attendance);
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      // jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 3600 }, (err, token) => {
+      //   if (err) throw err;
+      //   // On your server-side when setting the JWT:
+      //   // res.cookie("token", token, { httpOnly: true, secure: true }).status(200); // Use `secure: true` in production with HTTPS
+      //   res.cookie("token", token, { httpOnly: true, secure: true, path: "/", domain: "localhost" }).status(200);
+      // });
+      jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 3600 }, (err, token) => {
+        if (err) {
+          console.error("Error signing token:", err);
+          return res.status(500).send("Server Error");
+        }
+        res.cookie("token", token, { httpOnly: true, secure: true }); // secure: true should be used in production with HTTPS
+        return res.status(200).json({ token });
+      });
     }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch) {
-      return res.status(400).json({ msg: "Invalid credentials" });
-    }
-    //add login time
-    const userOffset = user.timezone;
-    const phTimeLogin = moment().tz('Asia/Singapore').add(8, 'hours').format('YYYY-MM-DD HH:mm:ss');
-    const tzConverter =- 8;
-    const userTimeLogin = moment().utcOffset(userOffset * 60).subtract(tzConverter, 'hours').format('YYYY-MM-DD HH:mm:ss');
-    const attendance = new Attendance({
-      user: user.id,
-      loginTime: userTimeLogin,
-      phTimeLogin: phTimeLogin,
-      logoutTime: null,
-      phTimeLogout: null,
-      totalTime: 0,
-    });
-
-
-    //add entry to attendance table
-    await attendance.save();
-    
-    console.log("Updated attendance record:", attendance);
-
-    const payload = {
-      user: {
-        id: user.id,
-      },
-    };
-
-    jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 3600 }, (err, token) => {
-      if (err) throw err;
-      res.json({ token });
-    });
   } catch (err) {
     console.error(err.message);
     console.log(err);
-    res.status(500).send("Server Error");
-    
+    res.status(500).send("Server Error", err);
   }
 });
 
@@ -108,9 +119,12 @@ router.post("/logout", async (req, res) => {
 
     // add logout time
     const userOffset = user.timezone;
-    const phTimeLogout = moment().tz('Asia/Singapore').add(8, 'hours').format('YYYY-MM-DD HH:mm:ss');
-    const tzConverter =- 8;
-    const userTimeLogout = moment().utcOffset(userOffset * 60).subtract(tzConverter, 'hours').format('YYYY-MM-DD HH:mm:ss');
+    const phTimeLogout = moment().tz("Asia/Singapore").add(8, "hours").format("YYYY-MM-DD HH:mm:ss");
+    const tzConverter = -8;
+    const userTimeLogout = moment()
+      .utcOffset(userOffset * 60)
+      .subtract(tzConverter, "hours")
+      .format("YYYY-MM-DD HH:mm:ss");
 
     console.log("phTimeLogout:", phTimeLogout);
     console.log("userTimeLogout:", userTimeLogout);
@@ -125,11 +139,10 @@ router.post("/logout", async (req, res) => {
     attendance.logoutTime = userTimeLogout;
     attendance.phTimeLogout = phTimeLogout;
     attendance.totalTime = totalTime;
-    
+
     await attendance.save();
 
     console.log("Updated attendance record:", attendance);
-
 
     res.json({ msg: "User logged out successfully" });
   } catch (err) {
@@ -145,6 +158,20 @@ router.get("/register", (req, res) => {
 
 router.get("/logout", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/pages/logout.html"));
+});
+
+router.get("/validate-token", (req, res) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({ valid: false, msg: "No token provided" });
+  }
+
+  try {
+    jwt.verify(token, process.env.JWT_SECRET);
+    res.json({ valid: true });
+  } catch (error) {
+    res.status(401).json({ valid: false, msg: "Invalid token" });
+  }
 });
 
 module.exports = router;
