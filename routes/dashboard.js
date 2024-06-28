d47c08da71e66e795293390005ec6acb3f62f1b7
Jero-BFT <jerod@bigforktech.com>
2024-06-28 20:36:24 +0800

    KAN-12
    secured dashboard.js by adding the authMiddleware on the dashboard route

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const authMiddleware = require("../middlewares/authMiddleware");
 
-router.get("/dashboard", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/pages/dashboard.html"));
+router.get("/dashboard", authMiddleware, (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/pages/private/dashboard.html"));
 });
 
 module.exports = router;
