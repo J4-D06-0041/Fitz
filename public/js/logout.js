573dba63fddd491c53697f2c83b582c9ecf7de96
jeroCls <jero@clockworkdelivery.com>
2024-07-01 07:30:57 -0600

    push requested by Jero
    Hello Kirk gn pa push ni manong mo jero ang amo ni nga changes

--- public/js/logout.js
+++ public/js/logout.js
@@ -33,20 +33,27 @@ async function fetchUserDetails() {
 
 document.getElementById("logoutLink").addEventListener("click", async function () {
   try { 
-  const userDetails = await fetchUserDetails();
-  console.log("userDetails", userDetails);
-  setInterval(() => updateTime(userDetails.user.clientTimezone, userDetails.user.userTimezone), 1000);
-  document.addEventListener("DOMContentLoaded", function () {
-    updateTime(timezone);
-  });
-  document.getElementById("logoutLink").addEventListener("click", function () {
-    const expirationDate = new Date();
-    expirationDate.setMinutes(expirationDate.getMinutes() - 1); 
-    const expirationDateString = expirationDate.toUTCString();
-    document.cookie = `token=; expires=${expirationDateString}; path=/;`;
-    window.location.href = "/";
-  });
-}catch (error){
-  console.error("Logout Error: ", error);
-}
+    const userDetails = await fetchUserDetails();
+    console.log("userDetails", userDetails);
+    setInterval(() => updateTime(userDetails.user.clientTimezone, userDetails.user.userTimezone), 1000);
+    document.addEventListener("DOMContentLoaded", function () {
+      updateTime(timezone);
+      document.getElementById("logoutBtnId").addEventListener("click", async function () {
+
+        try {
+          const response = await axios.get("/api/auth/set-cookie");
+          console.log('response', response);
+          if (!response.data) {
+            throw new Error("Failed to fetch user timezone");
+          }
+          return response.data;
+        } catch (error) {
+          console.error("Error fetching user timezone:", error);
+          return null;
+        }
+      });
+    });
+  }catch (error){
+    console.error("Logout Error: ", error);
+  }
 });
