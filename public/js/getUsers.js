bdc06a45eeb057a946713315fc04ffb94df9ca36
Kirk Dungog <kirkdungog23@gmail.com>
2024-07-03 02:03:55 +0800

    Added deleteBtn for every row
    to be continued... not yet saving to the user model. as well as haven't added an edit button. testing out delete first.

--- public/js/getUsers.js
+++ public/js/getUsers.js
@@ -11,7 +11,7 @@ async function usersTable() {
 
     const usersTable = document.getElementById("users-table");
     const tbody = usersTable.querySelector(".tbody");
-
+    
     users.forEach(user => {
       const row = document.createElement("tr");
       row.innerHTML = `
@@ -20,7 +20,17 @@ async function usersTable() {
         <td>${user.username}</td>
         <td>${user.email}</td>
         <td>${user.role}</td>
+        <td>${user.userTimezone}</td>
+        <td>${user.clientTimezone}</td>
+        <td>${user.dateCreated}</td>
+        <td><button class="delete-btn">Delete</button></td>
       `;
+      document.querySelectorAll('.delete-btn').forEach((deleteBtn) => { 
+      deleteBtn.addEventListener('click', (event)=>{
+        const row = event.target.closest('tr');
+        row.remove();
+      });
+    });
       console.log(users)
       tbody.appendChild(row);
     });
